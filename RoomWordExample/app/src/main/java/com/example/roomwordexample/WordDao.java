1ccbe4a28fc86e58e7505aae9d873d9850841fc6
Jose M. Catalasan <jose.catalasan@mobileappscompany.us>
2019-08-20 13:47:25 -0400

    initial commit. finished code lab example

---
+++ RoomWordExample/app/src/main/java/com/example/roomwordexample/WordDao.java
@@ -0,0 +1,22 @@
+package com.example.roomwordexample;
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface WordDao {
+
+//   @Insert(onConflict = OnConflictStrategy.REPLACE) way to handle conflicts if table has more than one row
+    @Insert
+    void insert(Word word);
+
+    @Query("DELETE FROM word_table")
+    void deleteAll();
+
+    @Query("SELECT * from word_table ORDER BY word ASC")
+    LiveData<List<Word>> getAllWords();
+
+}
